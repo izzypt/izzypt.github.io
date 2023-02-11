39101eb10b8fd23deb214719e839c871644519d6
izzypt <bonvalots@gmail.com>
2023-02-11 19:38:16 +0000

    Updated images and email functionality

--- app.js
+++ app.js
@@ -11,3 +11,20 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+document.querySelector("#sendEmailButton").addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    const nameInput = document.querySelector(".contact-form .i-c-2 input[type='text']");
+    const emailInput = document.querySelector(".contact-form .i-c-2 input[type='email']");
+    const subjectInput = document.querySelector(".contact-form .input-control input[type='text']");
+    const messageInput = document.querySelector(".contact-form .input-control textarea");
+  
+    const name = encodeURIComponent(nameInput.value);
+    const email = encodeURIComponent(emailInput.value);
+    const subject = encodeURIComponent(subjectInput.value);
+    const message = encodeURIComponent(messageInput.value);
+  
+    const mailto = `mailto:smmbonvalot@gmail.com?subject=${subject}&body=Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+    window.location.href = mailto;
+  });
